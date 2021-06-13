d1249169234ffc643b3797be2e4762c2b83df268
Daniel Sieradski <daniel@self.agency>
2021-06-13 18:36:29 -0400

    wordpress example

---
+++ wordpress-example/wp-config.php
@@ -0,0 +1,34 @@
+<?php
+/* Set variables */
+
+foreach ($_ENV as $k => $v) {
+  define(str_replace('WORDPRESS_', '', $k), $v);
+}
+
+/* Check if dev or production env */
+$dev = $_ENV['ENVIRONMENT'] === 'development' ? true : false;
+
+/* WordPress general settings */
+$table_prefix = $_ENV['WORDPRESS_TABLE_PREFIX'];
+define('WP_DEBUG', $dev);
+define('SCRIPT_DEBUG', $dev);
+define('WP_DEBUG_LOG', true);
+
+$prodvars = array(
+  'COMPRESS_CSS', 'COMPRESS_SCRIPTS', 'ENFORCE_GZIP',
+  'WP_AUTO_UPDATE_CORE', 'DISALLOW_FILE_MODS', 'DISALLOW_FILE_EDIT'
+);
+
+foreach ($prodvars as $prodvar) {
+  define($prodvar, !$dev);
+}
+
+/* Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
+
+/* Set absolute path to WordPress and load settings */
+if (!defined('ABSPATH')) {
+  define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+require_once ABSPATH . 'wp-settings.php';
